9af3074f977908d2c808c03d3e6db4b55d31dd31
Alon Ganot <alon1ganot@gmail.com>
2024-04-27 17:27:12 +0300

    added title floating

--- frontend/src/pages/FormPage.tsx
+++ frontend/src/pages/FormPage.tsx
@@ -1,17 +1,21 @@
-import { Button, Grid, Icon } from "@mui/material"
+import { Button, Grid } from "@mui/material"
 import { Link } from "react-router-dom"
+import { Title, TopCenterTitle } from "../styles/SharedStyles"
 
 function FormPage() {
   return (
     <>
     <Grid container>
-        <Grid item xs={10} />
+        <Grid item xs={2} />
+        <Grid item xs={8}>
+          <TopCenterTitle height={10} variant="h5">אני מראה לך ארבע תמונות</TopCenterTitle>
+          <TopCenterTitle height={15} variant="h5">בבקשה תגיד.י לי באיזו תמונה רואים</TopCenterTitle>
+
+        </Grid>
         <Grid item xs={2}>
           <Button variant="outlined"><Link to="/">חזרה לדף הבית</Link></Button>
         </Grid>
-      </Grid>
-     <div>Hello form!</div>
-     <Icon>star</Icon>
+    </Grid>
     </>
   )
 }
--- frontend/src/styles/SharedStyles.tsx
+++ frontend/src/styles/SharedStyles.tsx
@@ -5,6 +5,14 @@ export const Title = styled(Typography)({
     fontWeight: 'bold !important',
   })
 
+export const TopCenterTitle = styled(Typography)<{ height: number }>((props) => ({
+    fontWeight: 'bold !important',
+    position: 'fixed',
+    top: `${props.height}%`,
+    left: '50%',
+    transform: `translate(-50%, -${props.height}%)`
+  }))
+
 export const Subtitle = styled(Typography)({
     fontWeight: 'bold !important',
     maxWidth: '35vw'
