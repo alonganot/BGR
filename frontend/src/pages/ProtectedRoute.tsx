aae547f7a73e749f02edb490b0e2e29351ae16a4
Alon Ganot <alon1ganot@gmail.com>
2024-05-25 15:53:01 +0300

    added go homepage button on protectedroute error

--- frontend/src/pages/ProtectedRoute.tsx
+++ frontend/src/pages/ProtectedRoute.tsx
@@ -1,12 +1,16 @@
 import { useAuthContext } from '../context/AuthContext';
 import { ReactNode } from 'react';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
-    const { isAuthenticated } = useAuthContext();
+    const { isAuthenticated, logout } = useAuthContext();
 
     if (!isAuthenticated) {
-        return <Typography variant='h2'>יש להתחבר דרך עמוד הבית</Typography>;
+        return <>
+        <Typography variant='h2' sx={{marginBottom: '2vh'}}>יש להתחבר דרך עמוד הבית</Typography>
+        <Button variant="outlined" onClick={()=> logout()}><Link to="/">חזרה לדף הבית</Link></Button>
+        </>
     }
 
     return children;
